43b539f104c8a8bfe64fefdf0a8f833a8ac08509
Ashley Frieze <ashley@incredible.org.uk>
2026-09-07 13:49:19 +0100

    Make the `StreamGenerator` an `AutoCloseable`
    This would allow for Stream generator types to be used with `try-with-resources`

--- pg/src/main/java/org/bouncycastle/openpgp/StreamGenerator.java
+++ pg/src/main/java/org/bouncycastle/openpgp/StreamGenerator.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 
 /**
  * Callback interface for generators that produce a stream to be informed when the stream has been
- * closed by the client.
+ * closed by the client. Complies with Java's own {@link AutoCloseable} interface
  */
-interface StreamGenerator
+interface StreamGenerator extends AutoCloseable
 {
     /**
      * Signal that the stream has been closed.
      */
+    @Override
     void close()
         throws IOException;
 }
